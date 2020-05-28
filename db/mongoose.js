1c578c4cf6953e8138ff7e285bb1eb51418f8a9b
Nik Till <niktill123@gmail.com>
2020-05-28 11:41:52 -0400

    npm package and mongodb update

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // connect to our database
-const mongoURI = process.env.MONGODB_URI ||'mongodb://localhost:27017/Soundcheck';
+const mongoURI = process.env.MONGODB_URI ||'mongodb://localhost:27017/soundcheck-database';
 
 mongoose.connect(mongoURI, { useNewUrlParser: true});
 
